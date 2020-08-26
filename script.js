ccff68be20e3653e2ef931b932114e1f4176c30b
sonamkesari <63420367+sonamkesari@users.noreply.github.com>
2020-08-26 14:28:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var sign_up_username;
+var sign_up_pass;
+function signup()
+{
+	document.getElementById('registerid').style.display="block";
+	document.getElementById('loginid').style.display ="none";
+}
+function login()
+{
+	document.getElementById('registerid').style.display="none";
+	document.getElementById('loginid').style.display ="block";
+}
+
